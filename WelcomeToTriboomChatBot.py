564e67cfd400bf23a1f88c61d48a0b2eb56c134f
Tyler Slijboom <tylerslijboom+github@gmail.com>
2026-08-06 10:20:23 -0400

    Updated to actually start the chat bot, still having Oauth issues

--- WelcomeToTriboomChatBot.py
+++ WelcomeToTriboomChatBot.py
@@ -1,43 +1,59 @@
 
 #from contextlib import asynccontextmanager
 #from fastapi import FastAPI
+from dotenv import load_dotenv
 from flask import ctx
+from os import getenv
+import twitchio
 from twitchio.ext import commands
 
-
-from dotenv import load_dotenv
-from os import getenv
+import logging
 
 class WelcomeToTriboomChatBot(commands.Bot):
-    def __init__(self) -> None:
+    def __init__(self, new_logger: logging.Logger | None = None) -> None:
+        self.login_name = "TriboomsChatBot"
+        self.logger = new_logger or logging.getLogger(__name__)
         super().__init__(
             client_id=getenv('CLIENT_ID'),
             client_secret=getenv('CLIENT_SECRET'),
-            bot_id=234,
+            bot_id=1226671483,
+            owner_id="tribooms",
             prefix='!',
-            redirect_uri='http://localhost:4343/oauth/callback',
+            redirect_uri='https://twitchtokengenerator.com/oauth/callback',
             channel="tribooms"
-
         )
 
-        print(f"{self.__class__.__name__} initialized")
-
+        self.logger.info(f"{self.__class__.__name__} initialized")
+            
     async def event_ready(self):
-        print(f'Logged into Twitch as {self}')
+        self.logger.info(f'Logged into Twitch !')
 
     async def get_status(self) -> str:
-        return f"{self.__class__.__name__} is running in channels: {', '.join(self.channel_list)}"
+        return f"{self.__class__.__name__} is running"
 
     async def say_hi(self) -> None:
+        self.logger.info(f"Saying Hello")
         print(f"Saying Hello")
         the_channel = await self.fetch_channel("tribooms")
         await the_channel.send( f"Hi Chat!" )
 
     @commands.command(name='hello')
     async def hello_command(self, ctx):
-        await ctx.send(f'Hello {ctx.author.name} from {self.__class__.__name__}')
+        await ctx.send(f"Hello {ctx.author.name} from {self.__class__.__name__}")
 
-    @commands.command(name='tribothelp')
-    async def tribothelp_command(self, ctx):
-        await ctx.send(f'Hello {ctx.author.name} from {self.__class__.__name__}')
+    @commands.command(name="user_info")
+    async def user_info(self, ctx):
+        # Only need to run once taken from twitchio docs
+        with twitchio.Client(client_id=getenv('CLIENT_ID'), client_secret=getenv('CLIENT_SECRET')) as client:
+            await client.login()
+            twitch_users = await client.fetch_users(logins=[ctx.author.name])
+            for user in twitch_users:
+                self.logger.debug(f"Twitch user '{self.login_name}' fetched. name: {user.name} (ID: {user.id})")
+        
+
+    @commands.command(name='uh_oh')
+    async def uh_oh(self, ctx):
+        import winsound
+        winsound.PlaySound("audio/", winsound.SND_FILENAME)
+        await ctx.send(f"User {ctx.author.name} used Audio Ping")
 
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 
-from dotenv import load_dotenv
-from os import getenv
 
 import asyncio
 import logging
-
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
-#from twitchio.ext import commands
-
-from twitchio.utils import setup_logging
-
 import WelcomeToTriboomChatBot 
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from os import getenv
+#from twitchio.ext import commands
+from twitchio.utils import setup_logging as setup_twitchio_logging
+from twitchio import Client as TwitchClient
 
 load_dotenv
+#setup_twitchio_logging(level=logging.DEBUG)
 
-setup_logging(level=logging.DEBUG)
 
-logger = logging.getLogger("uvicorn.error")
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG, filename="project.log",
+                    format="%(asctime)s - %(levelname)s - %(message)s" )
+logger = logging.getLogger(__name__)
+print("logger is made")
+logger.info("Logger initialized beep boop")
+#logger = logging.getLogger("uvicorn.error")            
 bot = WelcomeToTriboomChatBot.WelcomeToTriboomChatBot()
 
 # Helper function to avoid naming conflict with asyncio
@@ -27,22 +30,55 @@ def asyncio_create_task(temp_coroutine):
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    print("Going to start bot")
     bot_task = asyncio_create_task(bot.start())
+    print("Bot starting")
+    await bot.start()
+    print("Bot started")
     yield
     await bot.close()
+    print("Bot Closed")
+
 
 app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 async def root():
+    print(f"bot is made: {bot}")
     status_message = await bot.get_status()
+    return {"status": status_message}
+
+
+@app.get("/user_info")
+async def user_info():
+    status_message = ""
+    # Only need to run once taken from twitchio docs
+    async with TwitchClient(client_id=getenv('CLIENT_ID'), client_secret=getenv('CLIENT_SECRET')) as client:
+        await client.login()
+        print(f" Twitch client created {client}")
+        twitch_users = await client.fetch_users(logins=["TriboomsChatBot", "onetwofiveeleven", "tribooms"])
+        print(f"{len(twitch_users)} Twitch user(s) fetched for 'TriboomsChatBot'")
+        logger.debug(f"{len(twitch_users)} Twitch user(s) fetched for 'TriboomsChatBot'")
+        status_message = "0 Users found" if len(twitch_users) == 0 else ""
+        for user in twitch_users:
+            user_message = f"Twitch user fetched. name: {user.name} (ID: {user.id})"
+            print(user_message)
+            status_message += user_message + "\n"
+            logger.debug(user_message)
+    
+    #bot_task = asyncio_create_task(bot.user_info())
+    #await bot.user_info()
     return {"status": status_message}
 
 @app.get("/say_hi")
 async def say_hi():
     bot_task = asyncio_create_task(bot.say_hi())
     await bot.say_hi()
-    return {"status": "Said hello to chat" }
+    return {"status": "Said Hi Chat to chat" }
 
-
+@app.get("/uh_oh")
+async def uh_oh():
+    bot_task = asyncio_create_task(bot.uh_oh()) 
+    await bot.uh_oh()
+    return {"status": "Audio Pinged played and logged"}
